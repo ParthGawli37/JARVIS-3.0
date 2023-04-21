67bec18b7bf82c7b978058ab917fe7180f7b74fe
ParthGawli37 <130574282+ParthGawli37@users.noreply.github.com>
2023-04-21 15:58:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import openai
-from apikey import api_data
+from api_key import api_data
 import pyttsx3
 import speech_recognition as sr
 import webbrowser
-import datetime
 import sqlite3
+import random
 import requests
-from bs4 import BeautifulSoup
+from PIL import ImageGrab
+import subprocess
 import psutil
+import winsound
 
 openai.api_key=api_data
 
@@ -15,22 +17,100 @@ completion=openai.Completion()
 
 def Reply(question):
     prompt=f'Parth: {question}\n Jarvis: '
-    response=completion.create(prompt=prompt, engine="text-davinci-002", stop=['\Parth'], max_tokens=200)
+    response=completion.create(prompt=prompt, engine="text-davinci-002", stop=['\Parth'], max_tokens=124)
     answer=response.choices[0].text.strip()
     return answer
+
+def searchOnGoogle(query):
+    query = query.replace(" ", "+")
+    url = f"https://www.google.com/search?q={query}"
+    webbrowser.open(url)
+
+def searchOnYoutube():
+    speak("What video should I search?")
+    video = takeCommand()
+    query = video.replace(' ', '+')
+    url = f"https://www.youtube.com/results?search_query={query}"
+    webbrowser.get().open(url)
+    speak(f"Playing {video} on YouTube.")
+
+def set_alarm():
+    alarm_time_str = input("Enter the time in 'HH:MM:SS AM/PM' format: ")
+    speak("Enter the time in 'HH:MM:SS AM/PM' format: ")
+    try:
+        datetime.datetime.strptime(alarm_time_str, '%I:%M:%S %p')
+    except ValueError:
+        print("Invalid time format! Please enter time in 'HH:MM:SS AM/PM' format.")
+        speak("Invalid time format! Please enter time in 'HH:MM:SS AM/PM' format.")
+        return
+    current_time = datetime.datetime.now().strftime('%I:%M:%S %p')
+    print(f"Current time is {current_time}.")
+    speak(f"Current time is {current_time}.")
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now().strftime('%I:%M:%S %p')
+        if current_time == alarm_time_str:
+            speak("Time's up!")
+            print("Time's up!")
+            for i in range(3):
+                winsound.Beep(1000, 1000)
+            break
+
+def tell_joke():
+    jokes = [
+        "Why did the tomato turn red? Because it saw the salad dressing!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "Why did the cookie go to the doctor? Because it was feeling crummy!",
+        "Why did the teddy bear say no to dessert? Because it was already stuffed!",
+        "Why did the chicken cross the playground? To get to the other slide!"
+    ]
+    joke = random.choice(jokes)
+    speak(joke)
+
+def send_whatsapp_message(name):
+
+    contacts = {'pappa': '+919930008990', 'mummy': '+918779761584', 'ayush': '+917718068314', 'om': '9224477282'}
+    if name.lower() in contacts:
+        phone_number = contacts[name.lower()]
+
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("What do you want to say?")
+            audio = recognizer.listen(source)
+            message = recognizer.recognize_google(audio)
+
+        print(f"Sending message '{message}' to {name}")
+
+        pywhatkit.sendwhatmsg(phone_number, message, int(time.strftime("%H")), int(time.strftime("%M")) + 1, wait_time=20)
+    else:
+        print(f"Sorry, I don't have a contact named {name}.")
+
+def take_screenshot():
+    speak("Taking Screenshot")
+    screenshot = ImageGrab.grab()
+    screenshot.save("screenshot.png")
+    speak("Screenshot saved")
+
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Hello How Are You? ")
+
 conn = sqlite3.connect('voice_input.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE IF NOT EXISTS voice_input
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+c.execute('''CREATE TABLE IF NOT EXISTS voice_input 
+             (id INTEGER PRIMARY KEY AUTOINCREMENT, 
              input TEXT NOT NULL,
              timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
 conn.commit()
+
 
 def takeCommand():
     r = sr.Recognizer()
@@ -44,80 +124,15 @@ def takeCommand():
         print("Parth Said: {} \n".format(query))
         c.execute("INSERT INTO voice_input (input) VALUES (?)", (query,))
         conn.commit()
+
     except Exception as e:
         print("Say That Again....")
         return "None"
     return query
 
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
-
-def greet():
-    hour = datetime.datetime.now().hour
-    if hour>=0 and hour<12:
-        speak("Good Morning!")
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon!")
-    else:
-        speak("Good Evening!")
-    speak("Fetching weather information")
-    search = "temperature in kandivali"
-    url = f"https://www.google.com/search?q={search}"
-    r = requests.get(url)
-    data = BeautifulSoup(r.text, "html.parser")
-    temp = data.find("div", class_="BNeawe").text
-    speak(f"current {search} is {temp}")
-
-    battery = psutil.sensors_battery()
-    percent = battery.percent
-    speak(f"Sir, your device's battery percentage is {percent} percent.")
-    speak("How can I help you today?")
-
-
-def tellDateTime():
-    now = datetime.datetime.now()
-    date = now.strftime("%d-%m-%Y")
-    day = now.strftime("%A")
-    time = now.strftime("%I:%M %p")
-    month = now.strftime("%B")
-    speak("Today is " + date + " and it's " + day)
-    speak("The current time is " + time)
-    speak("The current month is " + month)
-
-greet()
-
-def google_search():
-    speak("What should I search for you, Sir?")
-    search_query = Take_query()
-    url = f'https://www.google.com.tr/search?q={search_query}'
-    webbrowser.open_new_tab(url)
-    speak("Here is what I found for " + search_query + " on Google.")
-
-def youtube_search():
-    speak("What video should I search?")
-    video = Take_query()
-    query = video.replace(' ', '+')
-    url = f"https://www.youtube.com/results?search_query={query}"
-    webbrowser.get().open(url)
-    speak(f"Playing {video} on YouTube.")
-
-def tell_joke():
-    jokes = [
-        "Why did the tomato turn red? Because it saw the salad dressing!",
-        "Why did the scarecrow win an award? Because he was outstanding in his field!",
-        "Why did the cookie go to the doctor? Because it was feeling crummy!",
-        "Why did the teddy bear say no to dessert? Because it was already stuffed!",
-        "Why did the chicken cross the playground? To get to the other slide!"
-    ]
-    joke = random.choice(jokes)
-    speak(joke)
-
-while True:
-    query=takeCommand().lower()
-    if 'time' in query or 'date' in query or 'day' in query or 'month' in query:
-        tellDateTime()
-    else:
+if __name__ == '__main__':
+    while True:
+        query=takeCommand().lower()
         ans=Reply(query)
         print(ans)
         speak(ans)
@@ -125,10 +140,18 @@ while True:
             webbrowser.open("www.youtube.com")
         elif 'open google' in query:
             webbrowser.open("www.google.com")
+        elif 'google search' in query:
+            query = query.replace("search", "")
+            searchOnGoogle(query)
+        elif 'youtube search' in query:
+            query = query.replace("search youtube", "")
+            searchOnYoutube()
         elif "open whatsapp" in query:
              subprocess.run(["C:\\Users\\Parth\\AppData\\Local\\WhatsApp\\WhatsApp.exe"])
-        elif "open Pycharm" in query:
+        elif "open code" in query:
             subprocess.run(["C:\\Program Files\\ldplayer9box\\New folder\\PyCharm Edu 2022.2.2\\bin\\pycharm64.exe"])
+        elif "send message" in query:
+            send_whatsapp_message(name)
         elif "open word" in query:
             os.startfile('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office\\Microsoft Word 2019.lnk')
         elif "open excel" in query:
@@ -139,22 +162,31 @@ while True:
             os.system('notepad')
         elif "open clock" in query:
             os.system('start ms-clock:')
-        elif "jarvis shutdown" in query:
-            shutdown()
         elif "open calculator" in query:
             os.system('calculator')
-        elif "play" in query:
-            youtube_search()
-        elif "joke" in query:
+        elif "open settings" in query:
+            open_settings()
+        elif "set alarm" in query:
+            set_alarm()
+        elif "tell me a joke" in query:
             tell_joke()
-        elif "screenshot" in query:
-            speak("Taking Screenshot")
-            screenshot = ImageGrab.grab()
-            screenshot.save("screenshot.png")
-            speak("Screenshot saved")
-        elif "search" in query:
-            google_search()
-
-        elif 'bye' in query:
+        elif "send whatsapp message to" in query:
+            name = query.split("to ")[1]
+            send_whatsapp_message(name)
+        elif "take a screenshot" in query:
+            take_screenshot()
+        elif "cpu usage" in query:
+            usage = str(psutil.cpu_percent())
+            speak('CPU usage is at ' + usage)
+        elif "battery status" in query:
+            battery = psutil.sensors_battery()
+            speak("Battery is at ")
+            speak(battery.percent)
+        elif "exit" in query:
             speak("Goodbye!")
-            break
+            exit()
+        else:
+            pass
+
+
+
